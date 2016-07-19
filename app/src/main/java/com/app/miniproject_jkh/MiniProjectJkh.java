889221c2b6a3b1a7063c3925a70ec8096c71ff5d
Kyuhwan Cho <17679@hanmail.net>
2016-07-19 17:33:16 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/app/miniproject_jkh/MiniProjectJkh.java
@@ -0,0 +1,13 @@
+package com.app.miniproject_jkh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiniProjectJkh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mini_project_jkh);
+    }
+}
